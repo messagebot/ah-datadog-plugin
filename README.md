3a984f27a1831d0fd02acbb5ae6c658474740829
Evan Tahler <evantahler@gmail.com>
2016-11-03 16:56:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# AH-DATADOG-PLUGIN
+
+## Install
+
+- `npm install ah-datadog-plugin`
+- `./node_modules/.bin/actionhero link --name ah-datadog-plugin`
+
+## Requirements
+
+- Ensure that `DATADOG_API_KEY` is set in your Environment (this is the only config)
---
+++ initializers/datadog.js
@@ -0,0 +1,74 @@
+if(process.env.DATADOG_API_KEY){
+
+  const datadog = require('datadog-metrics');
+  let timer;
+  let timeout = 5 * 1000;
+
+  module.exports = {
+    initialize: function(api, next){
+      let appName = api.config.general.serverName.replace(/\s/g, '-') + '-' + api.env;
+      let host = api.id.replace(/\s/g, '-');
+
+      api.datadog = new datadog.BufferedMetricsLogger({
+        apiKey: process.env.DATADOG_API_KEY,
+        host: host,
+        prefix: `${appName}.`,
+        flushIntervalSeconds: 15
+      });
+
+      /* Action Middleware */
+      api.actions.addMiddleware({
+        name: 'datadog action middleware',
+        global: true,
+        preProcessor: function(data, next){
+          data.datadog = { startTime: (new Date()).getTime() }
+          next();
+        },
+        postProcessor: function(data, next){
+          let duration = ((new Date).getTime() - data.datadog.startTime) / 1000;
+          api.datadog.histogram('actions._all.duration', duration);
+          api.datadog.histogram(`actions.${data.params.action}.duration`, duration);
+          next();
+        }
+      });
+
+      /* Task Middleware */
+      api.tasks.addMiddleware({
+        name: 'datadog task middleware',
+        global: true,
+        preProcessor: function(next){
+          let worker = this.worker;
+          worker.datadog = { startTime: (new Date()).getTime() }
+          next();
+        },
+        postProcessor: function(next){
+          let worker = this.worker;
+          let duration = ((new Date).getTime() - worker.datadog.startTime) / 1000;
+          api.datadog.histogram('tasks._all.duration', duration);
+          api.datadog.histogram(`tasks.${worker.job.class}.duration`, duration);
+          next();
+        }
+      });
+
+      return next();
+    },
+
+    start: function(api, next){
+      let collectMemoryStats = function(){
+        var memUsage = process.memoryUsage();
+        api.datadog.gauge('memory.rss', memUsage.rss);
+        api.datadog.gauge('memory.heapTotal', memUsage.heapTotal);
+        api.datadog.gauge('memory.heapUsed', memUsage.heapUsed);
+      };
+
+      timer = setInterval(collectMemoryStats, timeout);
+      return next();
+    },
+
+    stop: function(api, next){
+      clearInterval(timer);
+      return next();
+    }
+  };
+
+}
